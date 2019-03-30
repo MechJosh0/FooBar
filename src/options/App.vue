35f537ac6e5eab12527555b62ec0c02468a9d000
Josh Welham <josh@aluminati.net>
2019-03-30 20:03:20 +0000

    vuetify

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,7 +1,19 @@
 <template>
-	<div>
+	<v-app>
 		<p>options {{ defaultText }}</p>
-	</div>
+		<v-btn color="success">
+			Success
+		</v-btn>
+		<v-btn color="error">
+			Error
+		</v-btn>
+		<v-btn color="warning">
+			Warning
+		</v-btn>
+		<v-btn color="info">
+			Info
+		</v-btn>
+	</v-app>
 </template>
 
 <script>
--- src/standalone/router/pages/Index.vue
+++ src/standalone/router/pages/Index.vue
@@ -1,5 +1,36 @@
 <template>
-	<p>standalone {{ defaultText }}</p>
+	<div>
+		<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Vuetify</span>
+				<span class="font-weight-light">MATERIAL DESIGN</span>
+			</v-toolbar-title>
+			<v-spacer />
+			<v-btn
+				flat
+				href="https://github.com/vuetifyjs/vuetify/releases/latest"
+				target="_blank"
+			>
+				<span class="mr-2">Latest Release</span>
+			</v-btn>
+		</v-toolbar>
+
+		<v-content>
+			<p>standalone {{ defaultText }}</p>
+			<v-btn color="success">
+				Success
+			</v-btn>
+			<v-btn color="error">
+				Error
+			</v-btn>
+			<v-btn color="warning">
+				Warning
+			</v-btn>
+			<v-btn color="info">
+				Info
+			</v-btn>
+		</v-content>
+	</div>
 </template>
 
 <script>
