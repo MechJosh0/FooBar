a34c91050fb2fe98e24a32dfefe1241416b0092a
Josh Welham <josh@aluminati.net>
2019-04-07 17:01:34 +0100

    store server and main net switch

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -1,16 +1,60 @@
-const state = { };
+import storage from '@/utils/storage';
 
-const mutations = { };
+const state = {
+	mainNet: true,
+	server: {
+		name: 'nulsWorld',
+		url: 'https://nuls.world/'
+	}
+};
+
+const mutations = {
+	SET_MAIN_NET(state, payload)
+	{
+		state.mainNet = payload;
+
+		storage.set('mainNet', payload);
+	},
+	SET_SERVER(state, payload)
+	{
+		state.server = payload;
+
+		storage.set('server', JSON.stringify(payload));
+	}
+};
 
 const actions = {
 	load({ dispatch }, account)
 	{
+		dispatch('setServerFromStorage');
 		dispatch('i18n/setLocaleFromStorage', null, { root: true });
 		dispatch('account/loginFromStorage', null, { root: true });
+	},
+	setMainNet({ commit }, payload)
+	{
+		commit('SET_MAIN_NET', payload);
+	},
+	setServer({ commit }, payload)
+	{
+		commit('SET_SERVER', payload);
+	},
+	setServerFromStorage({ dispatch })
+	{
+		dispatch('setMainNet', storage.get('mainNet'));
+		dispatch('setServer', JSON.parse(storage.get('server'));
 	}
 };
 
-const getters = { };
+const getters = {
+	isMainNet: (state) =>
+	{
+		return state.mainNet === true;
+	},
+	getServerData: (state) =>
+	{
+		return state.server;
+	}
+};
 
 export default {
 	namespaced: true,
