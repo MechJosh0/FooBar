17fe772f18c8938783021c12d2284e21c8b9d1c6
Josh Welham <josh@aluminati.net>
2019-05-05 12:57:23 +0100

    resolved issue where active wasn't working and navigation will now direct the user to the same page but different account

--- src/standalone/components/app/navigation/Addresses.vue
+++ src/standalone/components/app/navigation/Addresses.vue
@@ -10,47 +10,42 @@
 			:name="a.address"
 			:label="a.name"
 			:to="{
-				name: 'account.wallet.user',
+				name: routePath,
 				params: {
 					account: a.name
 				}
 			}"
-			active
 		/>
 	</q-tabs>
 </template>
 
 <script>
 	export default {
-		data()
-		{
-			return {
-				activeAccount: null
-			};
-		},
 		computed: {
+			routePath()
+			{
+				return this.$route.name || 'account.wallet.user';
+			},
 			isLoggedIn()
 			{
 				return this.$store.getters['app/account/isLoggedIn'];
 			},
-			account()
-			{
-				return this.$store.getters['account/getActiveAccount'] || {};
+			activeAccount: {
+				get()
+				{
+					const activeAccount = this.$store.getters['account/getActiveAccount'] || {};
+
+					return activeAccount.address;
+				},
+				set()
+				{
+					// Do nothing
+				}
 			},
 			accounts()
 			{
 				return this.$store.getters['account/getAccounts'];
 			}
-		},
-		watch: {
-			account(newVal)
-			{
-				this.activeAccount = this.account.address;
-			}
-		},
-		mounted()
-		{
-			this.activeAccount = this.account.address;
 		}
 	};
 </script>
