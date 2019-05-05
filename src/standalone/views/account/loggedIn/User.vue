4a06fe62303f904d22b3e1f7ead8f48fdb3023f8
Josh Welham <josh@aluminati.net>
2019-05-05 12:56:21 +0100

    removed old debug

--- src/standalone/views/account/loggedIn/User.vue
+++ src/standalone/views/account/loggedIn/User.vue
@@ -6,30 +6,30 @@
 
 <script>
 	export default {
-		computed: {
-			account()
-			{
-				return this.$store.getters['account/getActiveAccount'];
-			}
-		},
-		mounted()
-		{
-			chrome.runtime.sendMessage({});
-		},
-		methods: {
-			createAccount()
-			{
-				this.$store.dispatch('account/createNewAccount');
-			},
-			deleteAccount()
-			{
-				this.$store.dispatch('account/logOut');
-			},
-			changeLocale(locale)
-			{
-				this.$store.dispatch('i18n/setLocale', locale);
-			}
-		}
+		// computed: {
+		// 	account()
+		// 	{
+		// 		return this.$store.getters['account/getActiveAccount'];
+		// 	}
+		// },
+		// mounted()
+		// {
+		// 	chrome.runtime.sendMessage({});
+		// },
+		// methods: {
+		// 	createAccount()
+		// 	{
+		// 		this.$store.dispatch('account/createNewAccount');
+		// 	},
+		// 	deleteAccount()
+		// 	{
+		// 		this.$store.dispatch('account/logOut');
+		// 	},
+		// 	changeLocale(locale)
+		// 	{
+		// 		this.$store.dispatch('i18n/setLocale', locale);
+		// 	}
+		// }
 	};
 </script>
 
