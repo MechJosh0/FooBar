0f3fa43cf37f9198a031abfa1f3b4ecdb49957e8
Josh Welham <josh@aluminati.net>
2019-05-08 20:02:24 +0100

    host fontawesome locally

--- src/plugins/quasar.js
+++ src/plugins/quasar.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import '@/styles/quasar.styl';
 import 'quasar/dist/quasar.ie.polyfills';
-import iconSet from 'quasar/icon-set/fontawesome-v5.js';
 import '@quasar/extras/roboto-font/roboto-font.css';
-import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
 import {
 	Quasar,
 	Ripple,
@@ -58,7 +56,6 @@ import {
 } from 'quasar';
 
 Vue.use(Quasar, {
-	iconSet,
 	components: {
 		QBtn,
 		QLayout,
